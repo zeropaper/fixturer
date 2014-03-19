b5e816863a0898d0c0e64c09a31327bd65762887
Valentin Vago <zeropaper@irata.ch>
2014-03-19 07:02:41 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,377 @@
+(function (root, factory) {
+  /* global define: false, exports: false, require: false */
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['underscore', 'underscore.string'], factory);
+  }
+  else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require('underscore'), require('underscore.string'));
+  }
+  else {
+    // Browser globals (root is window)
+    root.fixturer = factory(root._, root._.string);
+  }
+}(this, function (_, str) {
+  'use strict';
+  /**
+   * Fixture tools
+   * @exports {function} fixturer
+   */
+  _.str = str;
+
+  /**
+   * The text used for example string generation
+   * @private
+   */
+  var _loremIpsum = [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.',
+    'Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis. Quisque mi metus, ornare sit amet fermentum et, tincidunt et orci. Fusce eget orci a orci congue vestibulum. Ut dolor diam, elementum et vestibulum eu, porttitor vel elit. Curabitur venenatis pulvinar tellus gravida ornare. Sed et erat faucibus nunc euismod ultricies ut id justo. Nullam cursus suscipit nisi, et ultrices justo sodales nec. Fusce venenatis facilisis lectus ac semper. Aliquam at massa ipsum. Quisque bibendum purus convallis nulla ultrices ultricies. Nullam aliquam, mi eu aliquam tincidunt, purus velit laoreet tortor, viverra pretium nisi quam vitae mi. Fusce vel volutpat elit. Nam sagittis nisi dui.',
+    'Suspendisse lectus leo, consectetur in tempor sit amet, placerat quis neque. Etiam luctus porttitor lorem, sed suscipit est rutrum non. Curabitur lobortis nisl a enim congue semper. Aenean commodo ultrices imperdiet. Vestibulum ut justo vel sapien venenatis tincidunt. Phasellus eget dolor sit amet ipsum dapibus condimentum vitae quis lectus. Aliquam ut massa in turpis dapibus convallis. Praesent elit lacus, vestibulum at malesuada et, ornare et est. Ut augue nunc, sodales ut euismod non, adipiscing vitae orci. Mauris ut placerat justo. Mauris in ultricies enim. Quisque nec est eleifend nulla ultrices egestas quis ut quam. Donec sollicitudin lectus a mauris pulvinar id aliquam urna cursus. Cras quis ligula sem, vel elementum mi. Phasellus non ullamcorper urna.',
+    'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In euismod ultrices facilisis. Vestibulum porta sapien adipiscing augue congue id pretium lectus molestie. Proin quis dictum nisl. Morbi id quam sapien, sed vestibulum sem. Duis elementum rutrum mauris sed convallis. Proin vestibulum magna mi. Aenean tristique hendrerit magna, ac facilisis nulla hendrerit ut. Sed non tortor sodales quam auctor elementum. Donec hendrerit nunc eget elit pharetra pulvinar. Suspendisse id tempus tortor. Aenean luctus, elit commodo laoreet commodo, justo nisi consequat massa, sed vulputate quam urna quis eros. Donec vel.'
+  ].join('\n\n');
+
+  /**
+   * Lists of names used for name generation
+   * @private
+   */
+  var _names = {
+    first: {
+      female: [
+        'Sara',
+        'Faye',
+        'Omega',
+        'Tracee',
+        'Dominica',
+        'Evie',
+        'Madge',
+        'Faustina',
+        'Alline',
+        'Minnie',
+        'Jeneva',
+        'Stacia',
+        'Margarett',
+        'Ma',
+        'Edythe',
+        'Kathleen',
+        'Shoshana',
+        'Kristal',
+        'Vicenta',
+        'Chan',
+        'Yolando',
+        'Despina',
+        'Janis',
+        'Dionna',
+        'Lurlene',
+        'Nadia',
+        'Samella',
+        'Olimpia',
+        'Manie',
+        'Lashonda',
+        'Karol',
+        'Tamela',
+        'Sabine',
+        'Olene',
+        'Kacie',
+        'Lavelle',
+        'Keitha',
+        'Kathyrn',
+        'Florence',
+        'Marylynn',
+        'Shalanda',
+        'Oneida',
+        'Anita',
+        'Azalee',
+        'Eilene',
+        'Kassandra',
+        'Margo',
+        'Thi',
+        'Eleonore',
+        'Myrtle'
+      ],
+      male: [
+        'Raymond',
+        'Don',
+        'Refugio',
+        'Luis',
+        'Karl',
+        'Fredrick',
+        'Benito',
+        'Barton',
+        'Shayne',
+        'Donny',
+        'Clifton',
+        'Greg',
+        'Lindsay',
+        'Kevin',
+        'Mikel',
+        'Freddy',
+        'Marvin',
+        'Edmundo',
+        'Zachary',
+        'Eusebio',
+        'Mel',
+        'Lenny',
+        'Abe',
+        'Kenneth',
+        'Dale',
+        'David',
+        'Gene',
+        'Mose',
+        'Lynn',
+        'Sean',
+        'Troy',
+        'Marion',
+        'Lowell',
+        'Reed',
+        'Antwan',
+        'Giuseppe',
+        'Carrol',
+        'Jay',
+        'Cordell',
+        'Bryce',
+        'Roy',
+        'Jamel',
+        'Grover',
+        'Carmine',
+        'Manual',
+        'Sylvester',
+        'Brendon',
+        'Jude',
+        'Burt',
+        'Renaldo'
+      ]
+    },
+    last: [
+      'Lax',
+      'Reynoso',
+      'Tilly',
+      'Klug',
+      'Ernst',
+      'Vrabel',
+      'Banda',
+      'Lansberry',
+      'Lehmkuhl',
+      'Hanning',
+      'Oda',
+      'Trask',
+      'Russom',
+      'Angelos',
+      'Krawczyk',
+      'Dahmen',
+      'Keys',
+      'Cairo',
+      'Bothe',
+      'Novak',
+      'Austin',
+      'Subia',
+      'Serino',
+      'Backer',
+      'Reinert',
+      'Wasson',
+      'Lou',
+      'Lasso',
+      'Cape',
+      'Cockerham',
+      'Rapier',
+      'Armijo',
+      'Frisch',
+      'Jess',
+      'Kimbler',
+      'Haney',
+      'Shumaker',
+      'Anstett',
+      'Rusher',
+      'Voss',
+      'Eccles',
+      'Linzey',
+      'Cane',
+      'Claunch',
+      'Rouleau',
+      'Huneke',
+      'Marceau',
+      'Leisy',
+      'Mondy',
+      'Justis'
+    ]
+  };
+
+  /**
+   * Lists of genders used for name generation
+   * @private
+   */
+  var _genders = [
+    // I know... this isn't correct...
+    'female',
+    'male'
+  ];
+
+  /**
+   * Generates an object of functions to be called from the templates
+   * @private
+   * @returns {Object.<string, Function>}
+   */
+  function _functions() {
+    var functions = {};
+    _.each([
+      'loremIpsum',
+      'random',
+      'evilScript',
+      'evilLink',
+      'personName'
+    ], function(name) {
+      functions[name] = fixturer[name];
+    });
+    return functions;
+  }
+
+
+  /**
+   * A fixture generation tools
+   * @param {Object.<string, *>} definition - A hash of in which replacements are being made
+   * @param {Number} [count]                - The number of fixture objects to be generated
+   * @returns {Object|array}                - The fixture(s)
+   */
+  var fixturer = function(definition, count) {
+    count = count || 1;
+    if (count > 1) {
+      var fixtures = [];
+      for (var c = 0; c < count; c++) {
+        fixtures.push(fixturer(definition));
+      }
+      return fixtures;
+    }
+
+    var fixture;
+
+    if (_.isString(definition)) {
+      // console.info('definition is string', definition);
+      fixture = _.template(definition, _functions());
+    }
+    else if (_.isArray(definition)) {
+      fixture = _.map(definition, function(item) {
+        return fixturer(item);
+      });
+    }
+    else if (_.isObject(definition)) {
+      fixture = {};
+      _.each(definition, function(item, name) {
+        fixture[name] = fixturer(item);
+      });
+    }
+    else {
+      fixture = defintion;
+    }
+
+    if (_.isString(fixture)) {
+      var prefix = /^::(bool|number)::(.*)/.exec(fixture);
+      if (prefix) {
+        switch (prefix[1]) {
+          case 'bool':
+            fixture = !!prefix[2];
+            if (prefix[2] === '0') {
+              fixture = false;
+            }
+            break;
+          case 'number':
+            fixture = parseFloat(prefix[2] || 0, 10);
+            break;
+        }
+      }
+    }
+
+    return fixture;
+  };
+
+  /**
+   * Example text generation
+   * @returns {string} - A string to be used for fixtures 
+   */
+  fixturer.loremIpsum = function() {
+    return _loremIpsum;
+  };
+
+  /**
+   * Creates a random number
+   * @param {Number} min        - the minimal value return
+   * @param {Number} max        - the maximal value return
+   * @param {Number} precision  - the floating precision
+   * @returns {Number}
+   */
+  fixturer.random = function(min, max, precision) {
+    min = min || 0;
+    max = max || 100;
+    precision = precision || 0;
+    var rand = Math.random();
+    // not really elegant...
+    var diff = max - min;
+    var factor = Math.pow(10, precision);
+
+    return Math.round((min + (diff * rand)) * factor) / factor;
+  };
+
+  /**
+   * Evil code generation
+   * @returns {string} - A string with some evil code in it
+   */
+  fixturer.evilScript = function() {
+    return '<script>document.write(\'So screwed!\');</script>';
+  };
+
+  /**
+   * Evil link generation
+   * @returns {string} - A string with some evil link in it
+   */
+  fixturer.evilLink = function() {
+    return '<a href="http://evil.com>evil link</a>';
+  };
+
+
+  /**
+   * Pick one or more random items from an array
+   * @param {array} array - the source array
+   * @param {number} count - the number of items, 1 by default
+   * @returns {*}
+   */ 
+  fixturer.randItem = function(array, count) {
+    count = count || 1;
+    var items = [];
+
+    for (var c = 0; c < count; c++) {
+      items.push(array[[fixturer.random(0, array.length - 1)]]);
+    }
+
+    if (count === 1) {
+      return items[0];
+    }
+    return items;
+  };
+
+  /**
+   * Person name generation
+   * @param {bool} first      - if strictly `false`, no first name part
+   * @param {bool} last       - if strictly `false`, no last name part
+   * @param {bool} middle     - if truthy, adds a middle name part
+   * @param {string} gender   - can be 'male', 'female', random if not defined
+   * @returns {string}        - A string with some evil code in it
+   */
+  fixturer.personName = function(first, last, middle, gender) {
+    gender = gender || fixturer.randItem(_genders);
+    var name = [];
+    if (first !== false) {
+      name.push(fixturer.randItem(_names.first[gender]));
+    }
+    if (middle) {
+      name.push(fixturer.randItem(_names.first[gender]));
+    }
+    if (last !== false) {
+      name.push(fixturer.randItem(_names.last));
+    }
+    return name.join(' ');
+  };
+
+  return fixturer;
+}));
---
+++ test.js
@@ -0,0 +1,141 @@
+var expect = require('expect.js');
+var path = require('path');
+var definition = {
+  name: '<%= "stringy thingy" %>',
+  obj: {
+    key: 'value'
+  },
+  random: {
+    float: '<%= random(0, 1, 2) %>',
+    integer: '<%= random(10, 100) %>',
+    negative: '<%= random(-100, -10) %>'
+  },
+  names: {
+    noargs: '<%= personName() %>',
+    female: '<%= personName(true, true, true, "female") %>',
+    male: '<%= personName(true, true, true, "male") %>',
+    lastname: '<%= personName(false, true) %>'
+  },
+  loremIpsum: '<%= loremIpsum() %>',
+  evil: {
+    link: '<%= evilLink() %>',
+    script: '<%= evilScript() %>'
+  },
+  transformation: [
+    '::number::1',
+    '::number::0.44',
+    '::bool::1',
+    '::bool::0'
+  ]
+};
+
+describe('The fixturer module', function(done) {
+  var fixturer, json;
+
+  it('loads without blowing', function(done) {
+    expect(function() {
+      fixturer = require('./index.js');
+      done();
+    }).not.to.throwError(done);
+  });
+
+  it('takes a definition', function(done) {
+    json = null;
+    try {
+      json = fixturer(definition);
+      done();
+    }
+    catch (err) {
+      done(err);
+    }
+  });
+
+  it('makes a fixture', function(done) {
+    expect(json).to.be.an('object');
+    expect(json.name).to.be.a('string');
+    expect(json.obj).to.be.an('object');
+    expect(json.obj.key).to.be('value');
+    done();
+  });
+
+  describe('the set of functions', function() {
+    before(function(done) {
+      json = fixturer(definition);
+      done();
+    });
+
+    it('has a "random" utility', function(done) {      
+      expect((''+ json.random.float).split('.').length).to.be(2);
+      expect(json.random.float).to.be.above(0);
+      expect(json.random.float).to.be.below(1);
+
+      expect((''+ json.random.integer).split('.').length).to.be(1);
+      expect(json.random.integer >= 10).to.be.ok();
+      expect(json.random.integer <= 100).to.be.ok();
+
+      expect((''+ json.random.negative).split('.').length).to.be(1);
+      expect(json.random.negative >= -100).to.be.ok();
+      expect(json.random.negative <= -10).to.be.ok();
+
+      done();
+    });
+
+    it('has a "loremIpsum" utility', function(done) {
+      expect(json.loremIpsum).to.be.a('string');
+      var firstWords = 'Lorem ipsum';
+      expect(json.loremIpsum.slice(0, firstWords.length)).to.be(firstWords);
+      done();
+    });
+
+    it('has a "personName" utility', function(done) {
+      // console.info(json.names);
+      expect(json.names).to.be.an('object');
+      expect(json.names.noargs).to.be.a('string');
+      expect(json.names.female).to.be.a('string');
+      expect(json.names.male).to.be.a('string');
+      expect(json.names.lastname).to.be.a('string');
+      done();
+    });
+
+    it('has a "evilScript" utility', function(done) {      
+      expect(fixturer('<%= evilScript() %>')).to.be.a('string');
+      expect(json.evil.script).to.be.a('string');
+      done();
+    });
+
+    it('has a "evilLink" utility', function(done) {
+      expect(fixturer('<%= evilLink() %>')).to.be.a('string');
+      expect(json.evil.link).to.be.a('string');
+      done();
+    });
+  });
+
+  describe('the transformation prefix', function() {
+    before(function(done) {
+      json = fixturer(definition);
+      done();
+    });
+    
+    it('transforms into boolean', function(done) {
+      expect(fixturer('::bool::1')).to.be(true);
+      expect(fixturer('::bool::0')).to.be(false);
+      expect(fixturer('::bool::')).to.be(false);
+      expect(fixturer('::bool::<%= 1 %>')).to.be(true);
+      expect(json.transformation).to.be.an('array');
+      expect(json.transformation[2]).to.be.a('boolean');
+      expect(json.transformation[3]).to.be.a('boolean');
+      done();
+    });
+
+    it('transforms into number', function(done) {
+      expect(fixturer('::number::10')).to.be(10);
+      expect(fixturer('::number::0')).to.be(0);
+      expect(fixturer('::number::')).to.be(0);
+      expect(fixturer('::number::<%= 1.67 %>')).to.be(1.67);
+      expect(json.transformation).to.be.an('array');
+      expect(json.transformation[0]).to.be.a('number');
+      expect(json.transformation[1]).to.be.a('number');
+      done();
+    });
+  });
+});
